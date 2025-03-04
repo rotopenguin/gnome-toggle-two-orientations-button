afa884091865e701ba6ee4be0f6cfbb24a1158a0
rotopenguin <git@rotopenguin.net>
2025-03-04 15:35:02 -0500

    orientation-a can be set in prefs.js. Yay!
    Copy-paste the same code, change orientation-a to orientation-b. It doesn't work. Why?

--- toggle-two-orientations@rotopenguin.net/prefs.js
+++ toggle-two-orientations@rotopenguin.net/prefs.js
@@ -27,8 +27,6 @@ import { ExtensionPreferences } from "resource:///org/gnome/Shell/Extensions/js/
 
 export default class MyPreferences extends ExtensionPreferences {
   fillPreferencesWindow(window) {
-    console.log("Orientation: Did we get to prefs.js yet??? "); 
-    //is the problem that the ui won't load without a <title> tag on the AdwPreferencesPage (or AdwPreferencesGroup)? None of this shit is documented it's all stupid.
     window._settings = this.getSettings();
     window.set_default_size(360, 300);
 
@@ -37,7 +35,7 @@ export default class MyPreferences extends ExtensionPreferences {
     builder.add_from_file(`${this.path}/prefs dialog.ui`);
    
 
-   /* const toggleA = builder.get_object("orientation-a");
+   const toggleA = builder.get_object("orientation-a");
     toggleA.set_active(window._settings.get_boolean("orientation-a"));
 
     window._settings.bind(
@@ -46,72 +44,21 @@ export default class MyPreferences extends ExtensionPreferences {
       "active",
       Gio.SettingsBindFlags.DEFAULT,
     );
-*/
+
+    const toggleB = builder.get_object("orientation-b");
+    toggleB.set_active(window._settings.get_boolean("orientation-b"));
+
+    window._settings.bind(
+      "orientation-b",
+      toggleB,
+      "active",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
+
+
     const page = builder.get_object("MainPage");
     window.add(page);
   }
 
 }
 
-
-
-/* //ugh I give up
-  getPreferencesWidget() {
-    const builder = new Gtk.Builder();
-    builder.add_from_file(`${this.path}/prefs dialog.ui`);
-    const page = builder.get_object("MainPage");
-    page._settings=this.getSettings();
-
-    const orientation_a = builder.get_object("orientation-a");
-    orientation_a.set_active(page._settings.get_boolean("orientation-a"));
-    page._settings.bind(
-      "orientation-a",
-      orientation_a,
-      "active",
-      Gio.SettingsBindFlags.DEFAULT,
-    );
-
-    const orientation_b = builder.get_object("orientation-b");
-    orientation_b.set_active(page._settings.get_boolean("orientation-b"));
-    page._settings.bind(
-      "orientation-b",
-      orientation_b,
-      "active",
-      Gio.SettingsBindFlags.DEFAULT,
-    );
-
-    console.log("Orientation prefs Page is ", page);
-    return page;
-  }
-}
-*/
-
-
-/*
-import Adw from 'gi://Adw';
-import GObject from 'gi://GObject';
-
-import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
-
-const MyPreferencesWidget = GObject.registerClass(
-class MyPreferencesWidget extends Adw.PreferencesGroup {
-    constructor() {
-        super({
-            title: _('Section'),
-        });
-
-        this.add(new Adw.SwitchRow({
-            title: _('Title'),
-            subtitle: _('Subtitle'),
-            active: true,
-        }));
-    }
-});
-
-export default class MyPreferences extends ExtensionPreferences {
-    getPreferencesWidget() {
-        return new MyPreferencesWidget();
-    }
-} 
-
-*/
